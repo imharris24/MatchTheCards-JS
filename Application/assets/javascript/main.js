7b927092bec9199b687a6fea229f18e14ff388bb
Muhammad Harris <harris20014@gmail.com>
2022-09-22 14:00:32 +0500

    v0.0.31
    - rename assets

---
+++ Application/assets/javascript/main.js
@@ -0,0 +1,52 @@
+const cardArray = [
+    {
+        name: 'card01',
+        img: 'assets/img/card01.png',
+    },
+    {
+        name: 'card02',
+        img: 'assets/img/card02.png',
+    },
+    {
+        name: 'card03',
+        img: 'assets/img/card03.png',
+    },
+    {
+        name: 'card04',
+        img: 'assets/img/card04.png',
+    },
+    {
+        name: 'card05',
+        img: 'assets/img/card05.png',
+    },
+    {
+        name: 'card06',
+        img: 'assets/img/card06.png',
+    },
+    {
+        name: 'card01',
+        img: 'assets/img/card01.png',
+    },
+    {
+        name: 'card02',
+        img: 'assets/img/card02.png',
+    },
+    {
+        name: 'card03',
+        img: 'assets/img/card03.png',
+    },
+    {
+        name: 'card04',
+        img: 'assets/img/card04.png',
+    },
+    {
+        name: 'card05',
+        img: 'assets/img/card05.png',
+    },
+    {
+        name: 'card06',
+        img: 'assets/img/card06.png',
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random());
